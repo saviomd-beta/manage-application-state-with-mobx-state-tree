1615beee440eeb5c0d43a162517af9e296c42470
Sávio Mendes <saviomd@gmail.com>
2018-08-14 11:05:39 -0300

    Lesson 5

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../assets/santa-claus.png';
+import WishListView from './WishListView';
 
 class App extends Component {
   render() {
@@ -8,11 +8,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">WishList</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <WishListView wishList={this.props.wishList} />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './assets/index.css';
+import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { WishList } from './models/WishList';
+
+const wishList = WishList.create({
+  items: [
+    {
+      name: 'LEGO Mindstorms EV3',
+      price: 349.95,
+      image: 'xxx.png'
+    },
+    {
+      name: 'Miracles - C.S. Lewis',
+      price: 12.91,
+      image: 'xxx.png'
+    }
+  ]
+});
+
+ReactDOM.render(<App wishList={wishList} />, document.getElementById('root'));
+
+setInterval(() => {
+  wishList.items[0].changePrice(wishList.items[0].price + 1);
+}, 1000);
