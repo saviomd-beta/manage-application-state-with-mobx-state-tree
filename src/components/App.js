f19563d7fa2c0f0e6efc1570ac0ae451c8872ab3
Sávio Mendes <saviomd@gmail.com>
2018-08-15 17:42:12 -0300

    Lesson 15

--- src/components/App.js
+++ src/components/App.js
@@ -18,6 +18,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">WishList</h1>
         </header>
+        <button onClick={group.reload}>Reload</button>
         <select onChange={this.onSelectUser}>
           <option>- Select user -</option>
           {Array.from(group.users.values()).map(user => (
